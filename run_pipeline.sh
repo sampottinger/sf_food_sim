c714d276fb7f6d97b2cb0c84b91f0ffa72890ca3
A Samuel Pottinger <sam@gleap.org>
2022-12-17 22:50:23 +0000

    Fix clipping.

--- run_pipeline.sh
+++ run_pipeline.sh
@@ -1,5 +1,6 @@
-rm -r working
-mkdir working
+#rm -r working
+#mkdir working
+rm working/named_fenced.csv
 python -m luigi --module pipeline FenceTask --local-scheduler
 rm viz/combined.csv
 cp working/named_fenced.csv viz/combined.csv
