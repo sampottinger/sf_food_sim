27fc4aa9b41005fa87372402df81869793f29d1b
A Samuel Pottinger <sam@gleap.org>
2022-12-17 17:03:41 +0000

    Working pipeline.

--- run_pipeline.sh
+++ run_pipeline.sh
@@ -1,3 +1,5 @@
 rm -r working
 mkdir working
-python -m luigi --module pipeline PrepareAndCheckNamedFeaturesTask --local-scheduler
+python -m luigi --module pipeline DedupeNamedTask --local-scheduler
+rm viz/combined.csv
+cp working/named_dedupe.csv viz/combined.csv
